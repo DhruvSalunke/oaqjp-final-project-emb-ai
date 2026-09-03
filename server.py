c2e7a65cf8c5e38c911a0a7c24c0521a342a4f66
Dhruv Salunke <salunkedhruv2@gmail.com>
2026-09-03 03:58:04 -0400

    Improve server code quality

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Flask server for the Emotion Detection application."""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,11 +8,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_index_page():
+    """Render the main emotion detection page."""
     return render_template("index.html")
 
 
 @app.route("/emotionDetector")
 def emotion_detector_route():
+    """Analyze the text submitted by the user."""
     text_to_analyze = request.args.get("textToAnalyze")
 
     response = emotion_detector(text_to_analyze)
